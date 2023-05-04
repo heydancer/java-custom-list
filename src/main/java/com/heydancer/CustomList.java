6352f69fcd587a2ab976edf59c06c894b5b971ae
Aleksandr <a.ustiantcev@gmail.com>
2023-05-04 22:36:08 +0300

    feat: add CustomList interface

---
+++ src/main/java/com/heydancer/CustomList.java
@@ -0,0 +1,19 @@
+package com.heydancer;
+
+public interface CustomList<T> {
+    void add(int index, T e);
+
+    void addFirst(T e);
+
+    void addLast(T e);
+
+    T remove(int index);
+
+    T removeFirst();
+
+    T removeLast();
+
+    void reverse();
+
+    int size();
+}
